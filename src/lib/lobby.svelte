ef3329a28bd12f2714bb644f1baf790a1ccf682e
jaasonw <jayywong92@gmail.com>
2023-03-11 13:48:29 -0800

    apparently i just never implemented join logic

--- src/lib/lobby.svelte
+++ src/lib/lobby.svelte
@@ -15,6 +15,27 @@
     $gameId = game.id;
     goto("/game");
   };
+
+  const joinRoom = async () => {
+    try {
+      //if this works go to gameboard
+      const game = await pb.collection("games").getOne($gameId);
+      console.debug(`Joining room ${game.id}`);
+      console.log(game);
+      // room is not full
+      if (game.player_2.length == 0 || game.player_1[0] == $currentUser?.id) {
+        // check if player isnt already player 1
+        if (game.player_1[0] != $currentUser?.id) {
+          await pb.collection("games").update(game.id, {
+            player_2: [$currentUser?.id],
+          });
+        }
+        goto("/game");
+      } else throw Error("Room is full");
+    } catch (error) {
+      alert(`An error occured while joining room: ${error}`);
+    }
+  };
 </script>
 
 {#if $currentUser}
@@ -40,9 +61,7 @@
       <input
         type="submit"
         value="Join with code"
-        on:click={() => {
-          goto("/game");
-        }}
+        on:click={joinRoom}
         class="bg-white hover:bg-amber-900 text-amber-900 font-semibold hover:text-white py-1 px-4 border border-amber-900 hover:border-transparent rounded my-0 w-full mx-0"
       />
 
